377bdf3e762fe213e7cd85def1a49d5ceed3c4c9
João Agostinho <63238478+KrOOb-5@users.noreply.github.com>
2023-06-20 18:21:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react"
 import "./styles.css"
-//20:22 - https://www.youtube.com/watch?v=Rh3tobg7hEo
+
 export default function App() {
   const [newItem, setNewItem] = useState("")
- 
+  const [todos, setTodos] = useState([])
+
+   function handleSubmit(e) {
+    e.preventDefault()
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos, 
+      {id: crypto.randomUUID(), title:newItem, completed: false},
+      ]
+    })
+    setTodos ([
+      ...todos, 
+      {id: crypto.randomUUID(), title:newItem, completed: false},
+    ])
+  }
+
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item"> New item</label>
           <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
